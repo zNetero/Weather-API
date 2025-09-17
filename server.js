e2bd453f0ae10f87194ee444a4c1b880a425c14a
Giordano <giordanob056@gmail.com>
2025-09-17 18:25:26 +0200

    Improving cache with Redis

--- server.js
+++ server.js
@@ -2,25 +2,64 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const apiKey = process.env.DB_API_KEY;
+const redis = require('redis');
+const redisClient = redis.createClient();
 
 const PORT = 3000;
 
-app.get('/weather/:location', async(req,res)=>{
-    const { location } = req.params
-    const apiKey = process.env.DB_API_KEY;
-    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&contentType=json`;
-    try{
-    const response = await axios.get(apiUrl);
-    res.json(response.data);
-    }catch(error){
-        console.error('Error:',error.response?.data || error.message);
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('Redis Client Connected'));
+
+(async () => {
+    await redisClient.connect();
+})
+
+
+
+app.get('/weather/:location', async (req, res) => {
+    const { location } = req.params;
+
+    try {
+        const cachedData = await redisClient.get(location);
+
+        if (cachedData) {
+            console.log(`Cache HIT for: ${location}`);
+            return res.json(JSON.parse(cachedData));
+        }
+
+        console.log(`Cache MISS for: ${location}`);
+
+        const apiKey = process.env.DB_API_KEY;
+        const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}&contentType=json`;
+
+        const response = await axios.get(apiUrl);
+        const weatherData = response.data;
+
+        await redisClient.set(location, JSON.stringify(weatherData), {
+            EX: 900, // expires in 15minutes
+        });
+
+        res.json(weatherData);
+    } catch (error) {
+        console.error('Error:', error.response?.data || error.message);
+
         res.status(500).json({
-            message: 'Internal Server Error'
-        })
+            message: 'Internal Server Error',
+        });
     }
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-});
+
+const startServer = async () => {
+    try {
+        await redisClient.connect();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error('Error starting the server:', err);
+        process.exit(1);
+    }
+}
+
+startServer();
